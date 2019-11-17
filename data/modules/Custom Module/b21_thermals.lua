0662bad4ec050babbc17bc0dedce97cb83913d09
xp-soaring <thriplow@gmail.com>
2019-11-17 15:05:41 +0000

    ok for force control via debug dataref

--- data/modules/Custom Module/b21_thermals.lua
+++ data/modules/Custom Module/b21_thermals.lua
@@ -12,7 +12,9 @@ local DATAREF_ELEVATION = globalPropertyf("sim/flightmodel/position/elevation")
 local DATAREF_AIRSPEED_MPS = globalPropertyf("sim/flightmodel/position/true_airspeed")
 
 -- create global DataRefs we will WRITE (name, default, isNotPublished, isShared, isReadOnly)
-DATAREF_LIFT_MPS = createGlobalPropertyf("b21_thermals/lift_mps", 0.0, false, true, true)
+local DATAREF_LIFT_MPS = createGlobalPropertyf("b21_thermals/lift_mps", 0.0, false, true, true)
+local DATAREF_DRAG_N = globalPropertyf("sim/flightmodel/forces/faxil_plug_acf") -- force along fuselage
+local DATAREF_DEBUG = createGlobalPropertyf("b21_thermals/debug", 0.0, false, true, true)
 
 --
 -- ----------------------------------------------
@@ -25,10 +27,14 @@ local prev_time_s = 0
 -- previous altitude (float meters)
 local prev_alt_m = 0
 
+local current_lift_mps = 0.0
+
 local init_completed = false
 
 function init()
-	print("b21_thermals initialized")
+	current_lift_mps = 2.0
+	set(DATAREF_LIFT_MPS, current_lift_mps)
+	print("b21_thermals initialized at", get(DATAREF_LIFT_MPS))
 end
 
 function update()
@@ -42,11 +48,20 @@ function update()
 	-- calculate time (float seconds) since previous update
 	local time_delta_s = get(DATAREF_TIME_S) - prev_time_s
 	
-	-- only update max 20 times per second (i.e. time delta > 0.05 seconds)
-	if time_delta_s > 1.05
+	-- only update max 1 times per second (i.e. time delta > 1 seconds)
+	if time_delta_s > 1.0
 	then
-		set(DATAREF_LIFT_MPS,7.7)
+		--current_lift = 2.0
+
+		--set(DATAREF_LIFT_MPS, current_lift)
 		prev_time_s = get(DATAREF_TIME_S)
 	end
+
+	local current_drag_n = get(DATAREF_DRAG_N)
+
+	--set(DATAREF_DEBUG, current_drag_n)
+
+	-- add negative force to DATAREF_DRAG_N
+	set(DATAREF_DRAG_N, current_drag_n - get(DATAREF_DEBUG) )
 		
 end -- function
